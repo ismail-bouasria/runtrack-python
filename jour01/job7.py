d3a34dede579736b4abbd8efe370c2c13a3c39eb
ismail <ismail.bouasria@laplateforme.io>
2023-03-30 11:39:21 +0200

    job7

---
+++ jour01/job7.py
@@ -0,0 +1,6 @@
+#job7
+def add(x, y):
+    result = x+y
+    return result
+
+print(add(2,2))
--- jour01/main.py
+++ jour01/main.py
@@ -6,15 +6,10 @@ print(10 - 3)
 print(10 / 3) 
 print(10 // 3)
 
-#job7
-def add(x, y):
-    result = x+y
-    return result
 
-print(add(2,2))
 
 #job11 
-def your_name(name):
+input(name)
     your_name = input(name)
     message = "hello,"+your_name
     return message
